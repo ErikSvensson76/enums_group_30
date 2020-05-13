71b70bc969ef0fe1094d89ac26cdca553e8bfabb
Erik Svensson <eosvensson@gmail.com>
2020-05-13 09:57:39 +0200

    Basic enum

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.example.util.WeekDay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        WeekDay[] weekDays = WeekDay.values();
+        for(WeekDay day : weekDays){
+            System.out.println(day);
+        }
+
+        System.out.println(convertFromString("monday"));
+    }
+
+    public static WeekDay convertFromString(String weekDay){
+        return WeekDay.valueOf(weekDay.toUpperCase());
+    }
+}
---
+++ src/main/java/org/example/util/WeekDay.java
@@ -0,0 +1,5 @@
+package org.example.util;
+
+public enum WeekDay {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+}
